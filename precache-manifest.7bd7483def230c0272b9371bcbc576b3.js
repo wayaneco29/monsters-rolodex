b98c20714b8a526bdbbb6a0f3fbe53cae22a539f
Jovany Steven Navares <jovany.steven.navares@gmail.com>
2019-07-18 00:05:42 +0800

    Updates

---
+++ precache-manifest.7bd7483def230c0272b9371bcbc576b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a648dddd0f72070b4e5cfdc8e808dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "20f43b556c8cd2f46fd7",
+    "url": "/monsters-rolodex/static/css/main.f50d2aa3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "20f43b556c8cd2f46fd7",
+    "url": "/monsters-rolodex/static/js/main.c49eeae1.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
